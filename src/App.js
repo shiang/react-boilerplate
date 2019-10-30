c8e2f1ba11caf0c637e5afab19ee62921ba2d988
shiang <ryan@launchlab.com.au>
2019-10-30 11:07:33 +1100

    - Split webpack settings into base/dev/prod, and add some required loaders, dynamic import and HMR - Add eslint/prettier and their configs - Update babel config - Add jest and its config

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import { hot } from 'react-hot-loader'
+
+const Warning = React.lazy(() => import('./Warning'))
+
+function App() {
+  const [count, setCount] = useState(0)
+  const increment = () => {
+    setCount(count + 1)
+  }
+
+  const decrement = () => {
+    setCount(count - 1)
+  }
+  return (
+    <div>
+      <h1>Hello World!!!</h1>
+      <div className={count > 10 ? 'warning' : null}>{count}</div>
+      <button type='button' onClick={increment}>
+        +
+      </button>
+      <button type='button' onClick={decrement}>
+        -
+      </button>
+      {count > 10 ? (
+        <React.Suspense fallback={null}>
+          <Warning />
+        </React.Suspense>
+      ) : null}
+    </div>
+  )
+}
+
+export default hot(module)(App)
